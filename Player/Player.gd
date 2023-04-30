435a6ec660619381c34dacf4e896afd0138de4ca
shinanhuang <122583606+shinanhuang@users.noreply.github.com>
2023-04-29 23:28:50 -0400

    Upload1

---
+++ Player/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody
+
+onready var Camera = get_node("/root/Game/Player/Pivot/Camera")
+
+var velocity = Vector3()
+var gravity = -9.0
+var speed = 0.2
+var max_speed = 4
+var mouse_sensitivity = 0.002
+
+func _physics_process(_delta):
+	velocity.y += gravity * _delta
+	var falling = velocity.y 
+	velocity.y = 0 
+	var desired_velocity = get_input() * speed
+	if desired_velocity.length():
+		velocity += desired_velocity
+	else:
+		velocity *= 0.9
+	var current_speed = velocity.length()
+	velocity = velocity.normalized() * clamp(current_speed,0,max_speed)
+	velocity.y = falling
+	
+	$AnimationTree.set("parameters/Idle_Run/blend_amount", current_speed/max_speed)
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+	
+func get_input():
+	var input_dir = Vector3()
+	if Input.is_action_just_pressed("forward"):
+		input_dir -= Camera.global_transform.basis.z
+	if Input.is_action_just_pressed("back"):
+		input_dir += Camera.global_transform.basis.z
+	if Input.is_action_just_pressed("left"):
+		input_dir -= Camera.global_transform.basis.x
+	if Input.is_action_just_pressed("right"):
+		input_dir += Camera.global_transform.basis.x
+	input_dir = input_dir.normalized()
+	return input_dir
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# 06-3D-RPG
-Project 6 of C220
+# 3D-Character
+
+An animated 3D character that can be used in the 3D-RPG project
+
+Based on the [Kenney Character Assets](https://kenney.itch.io/kenney-character-assets) provided by kenney.nl. The model and animations were compiled in Blender and then imported into Godot.
+
+## Implementation
+
+Created in Blender 3.5 and Godot 3.5
+
+## Created By
+
+Jason Francis
