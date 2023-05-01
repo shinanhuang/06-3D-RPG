2809631813ac445c8700fe30dc742f066976fea5
shinanhuang <122583606+shinanhuang@users.noreply.github.com>
2023-05-01 08:03:53 -0400

    upload10

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -1,6 +1,14 @@
 extends KinematicBody
 
+onready var Dialogue = get_node("/root/Game/UI/Diaglogue")
 
+var dialogue = [
+	"Welcome to the game! (Press E to continue)"
+	,"Your life depends on your speed and accuracy."
+	,"Shoot the five targets and defeat the drone before you run out of time!"
+	,"The challenge starts as soon as you press E"
+	
+]
 func _ready():
 	$AnimationPlayer.play("Idle")
 	
